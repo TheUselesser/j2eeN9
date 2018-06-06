d36966e23344ae3d95e13f3d69977a485b950aea
Thong <14520900@gm.uit.edu.vn>
2018-06-06 12:30:09 +0700

    Dumacia Finished

---
+++ backend/src/main/java/com/j2eeN9/backend/App.java
@@ -0,0 +1,13 @@
+package com.j2eeN9.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
